5b7ca115df99e476635a68e1c48971f50360b6b4
Jo Stichbury <jo_stichbury@mckinsey.com>
2022-12-20 17:36:04 +0000

    Update

--- kedro-tutorial/src/kedro_tutorial/pipelines/reporting/nodes.py
+++ kedro-tutorial/src/kedro_tutorial/pipelines/reporting/nodes.py
@@ -5,6 +5,8 @@ generated using Kedro 0.18.3
 import plotly.express as px
 import plotly.graph_objs as go
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
 
 def compare_passenger_capacity(preprocessed_shuttles: pd.DataFrame):
     return preprocessed_shuttles.groupby(["shuttle_type"]).mean().reset_index()
@@ -32,3 +34,16 @@ def compare_passenger_capacity(preprocessed_shuttles: pd.DataFrame):
         ]
     )
     return fig
+
+
+
+def create_confusion_matrix(companies: pd.DataFrame):
+    actuals = [0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1]
+    predicted = [1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1]
+    data = {"y_Actual": actuals, "y_Predicted": predicted}
+    df = pd.DataFrame(data, columns=["y_Actual", "y_Predicted"])
+    confusion_matrix = pd.crosstab(
+        df["y_Actual"], df["y_Predicted"], rownames=["Actual"], colnames=["Predicted"]
+    )
+    sn.heatmap(confusion_matrix, annot=True)
+    return plt
--- kedro-tutorial/src/kedro_tutorial/pipelines/reporting/pipeline.py
+++ kedro-tutorial/src/kedro_tutorial/pipelines/reporting/pipeline.py
@@ -5,6 +5,7 @@ generated using Kedro 0.18.3
 
 from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import compare_passenger_capacity
+from .nodes import create_confusion_matrix
 
 def create_pipeline(**kwargs) -> Pipeline:
     """This is a simple pipeline which generates a plot"""
@@ -14,6 +15,11 @@ def create_pipeline(**kwargs) -> Pipeline:
                 func=compare_passenger_capacity,
                 inputs="preprocessed_shuttles",
                 outputs="shuttle_passenger_capacity_plot",
+            ),
+            node(
+                func=create_confusion_matrix,
+                inputs="companies",
+                outputs="dummy_confusion_matrix",
             ),
         ]
     )
